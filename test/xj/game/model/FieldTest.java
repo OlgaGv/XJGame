dca27c9932b6e0e2bf69258f25360f1a53bec0d0
OlgaGv <olga844@mail.ru>
2020-02-14 18:42:43 +0300

    CheckOccupiedPoint

--- test/xj/game/model/FieldTest.java
+++ test/xj/game/model/FieldTest.java
@@ -89,4 +89,16 @@ class FieldTest {
         } catch (final InvalidPointException a) {}
 
     }
+
+    void testSetFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.X;
+        field.setFigure(inputPoint,inputFigure);
+        try {
+            field.setFigure(inputPoint,inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException a) {}
+
+    }
 }
