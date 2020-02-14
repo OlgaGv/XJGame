b047bbffefa01a813d32f7d6e04abbb7e9f3f3b6
OlgaGv <olga844@mail.ru>
2020-02-14 18:38:40 +0300

    Fiald checkPoints range

--- test/xj/game/model/FieldTest.java
+++ test/xj/game/model/FieldTest.java
@@ -1,19 +1,21 @@
 package xj.game.model;
 
 import org.junit.jupiter.api.Test;
+import xj.game.model.exceptions.AlreadyOccupiedException;
+import xj.game.model.exceptions.InvalidPointException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FieldTest {
 
     @Test
-    void getSize() {
+    void testgetSize() {
     final Field field = new Field();
     assertEquals(3, field.getSize());
 
     }
     @Test
-    void setFigure() {
+    void testsetFigure() throws Exception {
         final Field field = new Field();
         final Point inputPoint = new Point(0,0);
         final Figure inputFigure = Figure.X;
@@ -23,4 +25,68 @@ class FieldTest {
 
     }
 
+    void testGetFigureWhenFigureIsNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure actualFigure = field.getfigure(inputPoint);
+        assertNull(actualFigure);
+
+    }
+
+    void testGetFigureWhenXIsLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+        try {
+            field.getfigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException a) {}
+
+    }
+    void testGetFigureWhenYIsLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+        try {
+            field.getfigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException a) {}
+
+    }
+    void testGetFigureWhenXYIsLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,-1);
+        try {
+            field.getfigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException a) {}
+
+    }
+    void testGetFigureWhenXIsMoreThanFieldSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(5,0);
+        try {
+            field.getfigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException a) {}
+
+    }
+
+    void testGetFigureWhenYIsMoreThanFieldSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,5);
+        try {
+            field.getfigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException a) {}
+
+    }
+
+    void testGetFigureWhenXYIsMoreThanFieldSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(5,5);
+        try {
+            field.getfigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException a) {}
+
+    }
 }
