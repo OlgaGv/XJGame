5e2db480f787cd29ef4919e100ef934468d74cd3
OlgaGv <olga844@mail.ru>
2020-02-14 18:01:59 +0300

    Field and Point created

---
+++ src/xj/game/model/Field.java
@@ -0,0 +1,20 @@
+package xj.game.model;
+
+public class Field {
+
+    private static final int FIELD_SIZE=3;
+    private final Figure[][] field = new Figure [FIELD_SIZE][FIELD_SIZE];
+
+    public int getSize () {
+        return FIELD_SIZE;
+    }
+
+    public Figure getfigure(final Point point) {
+        return field[point.getX()][point.getY()];
+    }
+
+    public void setFigure(final Point point, final Figure figure) {
+        field[point.getX()][point.getY()] = figure;
+    }
+
+}
